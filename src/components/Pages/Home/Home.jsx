76806388c3fc11f01b64d38db855ea801af43722
webdevmahdi <mahdihasan01705259@gmail.com>
2023-10-17 16:00:18 +0600

    all pages setup

---
+++ src/components/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from '../../Shared/Header/Header'
+
+const Home = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Home
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './components/Pages/Home/Home.jsx'
+import Login from './components/Authentication/Login/Login.jsx'
+import Registration from './components/Authentication/Registration/Registration.jsx'
+import Main from './components/Layout/Main.jsx'
+
+let router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+      {
+        path: '/registration',
+        element: <Registration></Registration>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
